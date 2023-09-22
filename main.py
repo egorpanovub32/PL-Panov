6c587747826259e2a15aaca17d518557a0d2c9a6
egorpanovub32 <145374783+egorpanovub32@users.noreply.github.com>
2023-09-22 18:38:13 +0300

    git commit

---
+++ main.py
@@ -0,0 +1,9 @@
+a = int(input('Введите число'))
+b = int(input('Введите число'))
+c = int(input('Введите число'))
+if a >= 1 and a <= 3:
+    print(a)
+if b >= 1 and b <= 3:
+    print(b)
+if c >= 1 and c <= 3:
+    print(c)
