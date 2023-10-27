18c86c4cbfb7aa19e8f1784a6880f78955788571
egorpanovub32 <145374783+egorpanovub32@users.noreply.github.com>
2023-10-27 18:38:57 +0300

    Add files via upload

---
+++ 3/10.py
@@ -0,0 +1,7 @@
+a = int(input('Введите число'))
+b = int(input('Введите число'))
+c = int(input('Введите число'))
+if (a == b) and (a == c) and (b == c):
+    print('Числа равны')
+else:
+    print('Числа не равны')
