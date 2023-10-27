2e24b66b1c0aac7ef2f272265fd1ba9e68a39543
egorpanovub32 <145374783+egorpanovub32@users.noreply.github.com>
2023-10-27 18:41:35 +0300

    Delete Pr4 directory

--- Pr4/10.py
+++
@@ -1,7 +0,0 @@
-a = int(input('Введите число'))
-b = int(input('Введите число'))
-c = int(input('Введите число'))
-if (a == b) and (a == c) and (b == c):
-    print('Числа равны')
-else:
-    print('Числа не равны')
--- Pr4/5.py
+++
@@ -1,5 +0,0 @@
-a = int(input('Введите число'))
-b = int(input('Введите число'))
-c = int(input('Введите число'))
-sr = (a + b + c) / 3
-print(sr)
