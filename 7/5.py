bccd2207e5140a0366fcae64f50b4f92a8f1a03e
egorpanovub32 <145374783+egorpanovub32@users.noreply.github.com>
2023-11-16 17:39:58 +0300

    Add files via upload

---
+++ 7/5.py
@@ -0,0 +1,20 @@
+# 5.1
+# A = int(input())
+# B = int(input())
+# C = int(input())
+# D = int(input())
+# chisl = A * D + B * C
+# znam = B * D
+# a, b = chisl, znam
+# while a != b:
+#     if a > b:
+#         a -= b
+#     else:
+#         b -= a
+# print(f'{chisl // a}/{znam // a}')
+
+# 5.2
+# n = int(input())
+# for i in range(1, n + 1):
+#     if n % i == 0:
+#         print(i, end=" ")
